2df8ac6576cf70d5e29f91788fd7532bdb20777e
dimamohamad <deemaahmad189@gmail.com>
2024-07-16 23:31:08 +0300

    16-7-2024

--- 16-7-2024/SQLQuery1.sql
+++ 16-7-2024/SQLQuery1.sql
@@ -14,16 +14,19 @@ StudentName varchar(15),
 CourseID int foreign key REFERENCES Courses (CourseID) );
 
 CREATE TABLE Coursesstudents (CourseID int foreign key REFERENCES Courses (CourseID),
-StudentID int foreign key REFERENCES Students(StudentID));
+StudentID int foreign key REFERENCES Students(StudentID)
+ PRIMARY KEY (CourseID, StudentID));
 
-INSERT INTO Teachers VALUES(1,'dima'),(2,'lujain'),(3,'rahaf'),(4,'hosam'),(5,'yaman');
+INSERT INTO Teachers VALUES(1,'dima'),(2,'lujain'),(3,'rahaf'),(4,'ghaidaa'),(5,'suha');
 INSERT INTO  TeacherDetails VALUES (1,1000,1),(2,1000,2),(3,500,3),(4,4000,4),(5,150,5);
 INSERT INTO Courses VALUES (1,'c++',1),(2,'c#',2),(3,'Python',3),(4,'Html',4),(5,'asp.net',5);
-INSERT INTO Students VALUES (1,'Mohammad',1),(2,'yousef',2),(3,'omar',3),(4,'anas',4),(5,'ghaidaa',5);
+INSERT INTO Students VALUES (1,'Mohammad',1),(2,'yaman',2),(3,'omar',3),(4,'anas',4),(5,'hosam',5);
 INSERT INTO Coursesstudents VALUES (1,1),(2,2),(3,3),(4,4),(5,5);
 
 SELECT * FROM Teachers;
 SELECT * FROM TeacherDetails;
-SELECT * FROM Courses;
+SELECT CourseName,Tachear_name FROM Courses JOIN Teachers ON Courses.ID_teacher=Teachers.ID_teacher;
 SELECT * FROM Students;
 SELECT * FROM Coursesstudents;
+
+
