49ebf10f87497413649a437c6f9b0849d9b6ce65
dimamohamad <deemaahmad189@gmail.com>
2024-07-16 20:50:51 +0300

    16-7-2024

---
+++ 16-7-2024/SQLQuery1.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE SchoolSystem;
+USE SchoolSystem;
+CREATE TABLE Teachers(ID_teacher int primary key  ,
+Tachear_name char(10) );
+CREATE TABLE TeacherDetails(ID int primary key ,
+salary int , ID_teacher int UNIQUE foreign key REFERENCES Teachers (ID_teacher));
+CREATE TABLE Courses (CourseID int primary key  ,
+CourseName varchar(15),
+ID_teacher int  foreign key REFERENCES Teachers (ID_teacher)
+);
+
+Create TABLE Students(StudentID int primary key ,
+StudentName varchar(15),
+CourseID int foreign key REFERENCES Courses (CourseID) );
+
+CREATE TABLE Coursesstudents (CourseID int foreign key REFERENCES Courses (CourseID),
+StudentID int foreign key REFERENCES Students(StudentID));
+
+INSERT INTO Teachers VALUES(1,'dima'),(2,'lujain'),(3,'rahaf'),(4,'hosam'),(5,'yaman');
+INSERT INTO  TeacherDetails VALUES (1,1000,1),(2,1000,2),(3,500,3),(4,4000,4),(5,150,5);
+INSERT INTO Courses VALUES (1,'c++',1),(2,'c#',2),(3,'Python',3),(4,'Html',4),(5,'asp.net',5);
+INSERT INTO Students VALUES (1,'Mohammad',1),(2,'yousef',2),(3,'omar',3),(4,'anas',4),(5,'ghaidaa',5);
+INSERT INTO Coursesstudents VALUES (1,1),(2,2),(3,3),(4,4),(5,5);
+
+SELECT * FROM Teachers;
+SELECT * FROM TeacherDetails;
+SELECT * FROM Courses;
+SELECT * FROM Students;
+SELECT * FROM Coursesstudents;
